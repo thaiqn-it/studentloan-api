3606b5d22dae92cac2efa51ee520a814f0c256c3
larry-tran <longtran4949@gmail.com>
2022-01-20 22:17:05 +0700

    fix upload file

--- middlewares/uploadImage.js
+++ middlewares/uploadImage.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
 const uploadImage = async (req, res, next) => {
-  console.log(req.files);
+  // console.log(req.files);
   try {
     if (!req.files || Object.keys(req.files).length === 0)
       return res.status(400).json({ msg: "No files were uploaded." });
