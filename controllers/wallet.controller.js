6667a47c69df56b6a76f0607cba5947fd224926d
Trần Long <longtran4949@gmail.com>
2022-04-15 22:51:47 +0700

    fix bug repaymentAll

--- controllers/wallet.controller.js
+++ controllers/wallet.controller.js
@@ -64,7 +64,7 @@ const deleteById = async (req, res) => {
 const getByUserId = async (req, res) => {
   try {
     const user = req.user;
-    console.log(user)
+    console.log(user);
     const account = await walletService.getWalletByUserId(user);
     if (account.User.type === USER_TYPE.INVESTOR) {
       const totalPending = await InvestmentService.sumTotalPendingByInvetorId(
@@ -82,7 +82,7 @@ const getByUserId = async (req, res) => {
     }
     res.json(account);
   } catch (err) {
-    console.log(err)
+    console.log(err);
     res
       .status(INTERNAL_SERVER_ERROR)
       .json(restError.INTERNAL_SERVER_ERROR.default);
@@ -193,7 +193,7 @@ const repaymentAll = async (req, res) => {
 
     var studentWallet = null;
     var transaction = null;
-   await loanSchedules.map((loanSchedule) => {
+    await loanSchedules.map((loanSchedule) => {
       walletService.getWalletByUserId(user).then((resp) => {
         studentWallet = resp;
         walletService
@@ -268,14 +268,17 @@ const repaymentAll = async (req, res) => {
                       });
                   });
                   var data = { status: LOAN_SCHEDULE_STATUS.COMPLETED };
-                  loanScheduleService.updateById(loanSchedule.id, data);
+                  loanScheduleService
+                    .updateById(loanSchedule.id, data)
+                    .catch((err) =>
+                      res.json({ messge: "Thanh toán Thất bại", success: true })
+                    );
                 });
               });
           });
       });
     });
-    if(transaction == null)  throw new Error();
-    res.json(transaction);
+    res.json({ messge: "Thanh toán thành công", success: true });
   } catch (err) {
     res
       .status(INTERNAL_SERVER_ERROR)
