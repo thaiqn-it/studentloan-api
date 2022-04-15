1ad4454bcdb8800c130867015a8b12d83551a03a
Trần Long <longtran4949@gmail.com>
2022-04-15 17:21:40 +0700

    add loanMedia

--- controllers/loanMedia.controller.js
+++ controllers/loanMedia.controller.js
@@ -1,23 +1,61 @@
 const { loanMediaService } = require("../services/loanMedia.service");
-const { INTERNAL_SERVER_ERROR, NOT_FOUND,BAD_REQUEST } = require("http-status");
+const {
+  INTERNAL_SERVER_ERROR,
+  NOT_FOUND,
+  BAD_REQUEST,
+} = require("http-status");
 const { restError } = require("../errors/rest");
 const { mapErrorArrayExpressValidator } = require("../utils");
 const { validationResult } = require("express-validator");
-  
+const { LOANMEDIA_STATUS } = require("../models/enum");
+
 const findAllEvidenceByLoanId = async (req, res, next) => {
-    const { id } = req.params;
-    try {
-        const evidences = await loanMediaService.getAllEvidenceByLoanId(id);
-        if (evidences === null) throw new Error();
-		return res.json({
-            evidences
-		});
-    } catch (error) {
-        return res.status(NOT_FOUND).json(restError.NOT_FOUND.default());
-    }
+  const { id } = req.params;
+  try {
+    const evidences = await loanMediaService.getAllEvidenceByLoanId(id);
+    if (evidences === null) throw new Error();
+    return res.json({
+      evidences,
+    });
+  } catch (error) {
+    return res.status(NOT_FOUND).json(restError.NOT_FOUND.default());
+  }
+};
+
+const createLoanMedia = async (req, res) => {
+  try {
+    const data = req.body;
+    const newData = { ...data, status: LOANMEDIA_STATUS.ACTIVE };
+
+    const loanMedia = await loanMediaService.createLoanMedia(newData);
+    if(loanMedia == null) throw new Error()
+    res.json(loanMedia);
+  } catch (err) {
+    console.log(err);
+    res
+      .status(INTERNAL_SERVER_ERROR)
+      .json(restError.INTERNAL_SERVER_ERROR.default());
+  }
+};
+
+const updateLoanMedia = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = req.body;
+    const loanMedia = await loanMediaService.updateLoanMediaById(id, data);
+    if(loanMedia == null) throw new Error()
+    res.json(loanMedia);
+  } catch (err) {
+    console.log(err);
+    res
+      .status(INTERNAL_SERVER_ERROR)
+      .json(restError.INTERNAL_SERVER_ERROR.default());
+  }
 };
 
 
-exports.loanMediaController = { 
-    findAllEvidenceByLoanId
+exports.loanMediaController = {
+  findAllEvidenceByLoanId,
+  createLoanMedia,
+  updateLoanMedia,
 };
