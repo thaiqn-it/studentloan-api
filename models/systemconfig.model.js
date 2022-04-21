16123043f5f618d7c1633716da2fdb3a6fbcbf34
truongphi00 <kumayukito112@gmail.com>
2022-04-21 18:44:15 +0700

    edit model system config

--- models/systemconfig.model.js
+++ models/systemconfig.model.js
@@ -18,6 +18,18 @@ module.exports = (sequelize, DataTypes) => {
         penaltyFee:{
             type: DataTypes.FLOAT,
         },
+        minRaiseMoney:{
+            type: DataTypes.INTEGER,
+        },
+        maxRaiseMoney:{
+            type: DataTypes.INTEGER,
+        },
+        minDuration:{
+            type: DataTypes.INTEGER,
+        },
+        postExpireTime:{
+            type: DataTypes.INTEGER,
+        },
         status: {
             type: DataTypes.BOOLEAN,
         },
